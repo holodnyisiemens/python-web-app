9fc8a524e35026ad2721139ca1230d90d0a74057
holodnyisiemens <idolexey@yandex.ru>
2025-11-24 16:12:00 +0500

    Add Litestar app

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from litestar import Litestar
+from litestar.di import Provide
+
+from controllers.user_controller import UserController
+from di.providers import provide_db_session, provide_user_repository
+
+
+app = Litestar(
+    route_handlers=[UserController],
+    dependencies={
+        "db_session": Provide(provide_db_session),
+        "user_repository": Provide(provide_user_repository),
+    },
+    debug=True,
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
