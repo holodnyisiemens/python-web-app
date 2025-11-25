04f21c37f88f48ecda90eefb95396b519e41483e
holodnyisiemens <idolexey@yandex.ru>
2025-11-25 16:45:10 +0500

    Update UserService: add CRUD

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from uuid import UUID
 
 from litestar.exceptions import NotFoundException
@@ -11,10 +12,32 @@ class UserService:
     def __init__(self, user_repo: UserRepository):
         self.user_repo = user_repo
 
-
-    async def get_user_by_id(self, user_id: UUID) -> UserDTO:
+    async def get_by_id(self, user_id: UUID) -> UserDTO:
         user = await self.user_repo.get_by_id(user_id)
         if not user:
             raise NotFoundException(detail=f"User with ID {user_id} not found")
-        return UserDTO.model_validate(user, from_attributes=True)
+        return UserDTO.model_validate(user)
 
+    async def create(self, user_data: UserAddDTO) -> UserDTO:
+        user = await self.user_repo.create(user_data)
+        return UserDTO.model_validate(user)
+
+    async def delete(self, user_id: UUID) -> UserDTO:
+        user = await self.user_repo.delete(user_id)
+        if not user:
+            raise NotFoundException(detail=f"User with ID {user_id} not found")
+        return UserDTO.model_validate(user)
+
+    async def update(self, user_id: UUID, user_data: UserUpdateDTO) -> User:
+        user = await self.user_repo.update(user_id, user_data)
+        if not user:
+            raise NotFoundException(f"User with ID {user_id} not found")
+        return UserDTO.model_validate(user)
+    
+    async def get_user_count(self) -> int:
+        return await self.user_repo.get_user_count()
+
+    async def get_by_filter(self, count: Optional[int] = None, page: int = 1, **kwargs) -> list[UserDTO]:
+        users = await self.user_repo.get_by_filter(count, page, **kwargs)
+        user_dtos = [UserDTO.model_validate(user) for user in users]
+        return user_dtos
