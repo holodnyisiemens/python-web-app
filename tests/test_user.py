f33bec09f94960348572d3a04e10db3f0821c09b
holodnyisiemens <idolexey@yandex.ru>
2025-11-29 23:34:25 +0500

    test(user): edit test user data usage

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,48 +1,56 @@
 import pytest
 
-from schemas import UserAddDTO, UserUpdateDTO
+from schemas import UserAddDTO, UserUpdateDTO, UserDTO
 from repositories.user_repository import UserRepository
 
 
+user_data_1 = UserAddDTO(
+    username="john_doe",
+    email="john@example.com",
+    description="test user 1",
+)
+
+user_data_2 = UserAddDTO(
+    username="jason_smith",
+    email="jason@example.com",
+    description="test user 2",
+)
+
+
 class TestUserRepository:
-    async def _create_test_user(self, user_repository: UserRepository):
-        user_data = UserAddDTO(
-            username="john_doe",
-            email="test@example.com",
-            description="test user",
-        )
+    async def _create_test_user(self, user_repository: UserRepository, user_data: UserAddDTO) -> UserDTO:
         user = await user_repository.create(user_data)
-        return user, user_data    
+        return user
 
     @pytest.mark.asyncio
     async def test_create_user(self, user_repository: UserRepository):
-        user, user_data = await self._create_test_user(user_repository)
+        user = await self._create_test_user(user_repository, user_data_1)
 
         assert user.id is not None
-        assert user.email == user_data.email
-        assert user.username == user_data.username
-        assert user.description == user_data.description
+        assert user.email == user_data_1.email
+        assert user.username == user_data_1.username
+        assert user.description == user_data_1.description
 
     @pytest.mark.asyncio
     async def test_get_user_by_email(self, user_repository: UserRepository):
-        _, user_data = await self._create_test_user(user_repository)
+        await self._create_test_user(user_repository, user_data_1)
 
-        found_user = await user_repository.get_by_email(user_data.email)
+        found_user = await user_repository.get_by_email(user_data_1.email)
 
         assert found_user.id is not None
-        assert found_user.email == user_data.email
-        assert found_user.username == user_data.username
-        assert found_user.description == user_data.description
+        assert found_user.email == user_data_1.email
+        assert found_user.username == user_data_1.username
+        assert found_user.description == user_data_1.description
 
     @pytest.mark.asyncio
     async def test_update_user(self, user_repository: UserRepository):
-        user, user_data = await self._create_test_user(user_repository)
+        user = await self._create_test_user(user_repository, user_data_1)
 
         new_user_data = UserUpdateDTO(username="Updated")
 
         await user_repository.update(user.id, new_user_data)
 
-        updated_user = await user_repository.get_by_email(user_data.email)
+        updated_user = await user_repository.get_by_email(user_data_1.email)
         
         assert updated_user.username == new_user_data.username
 
@@ -52,10 +60,8 @@ class TestUserRepository:
 
     @pytest.mark.asyncio
     async def test_delete_user(self, user_repository: UserRepository):
-        user, user_data = await self._create_test_user(user_repository)
-
+        user = await self._create_test_user(user_repository, user_data_1)
         await user_repository.delete(user.id)
 
-        found_user = await user_repository.get_by_email(user_data.email)
-
+        found_user = await user_repository.get_by_email(user_data_1.email)
         assert found_user is None
