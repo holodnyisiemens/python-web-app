9b1a2e1134f2b70a132f088f49118a6d4bbb8b76
holodnyisiemens <idolexey@yandex.ru>
2025-12-09 01:35:01 +0500

    Edit cart repo tests

--- tests/test_cart_repository.py
+++ tests/test_cart_repository.py
@@ -1,129 +1,66 @@
 import pytest
 
-from schemas import CartAddDTO, CartUpdateDTO, CartDTO, AddressAddDTO, UserAddDTO, UserDTO, AddressDTO, ProductAddDTO, ProductDTO
+from schemas import CartAddDTO, CartUpdateDTO, CartDTO, UserDTO, ProductDTO
 from repositories.cart_repository import CartRepository
-from repositories.user_repository import UserRepository
-from repositories.address_repository import AddressRepository
-from repositories.product_repository import ProductRepository
-
-
-user_data_1 = UserAddDTO(
-    username="Alexey",
-    email="email@example.com",
-)
-
-user_data_2 = UserAddDTO(
-    username="Alexander",
-    email="another@example.com",
-)
-
-product_data_1 = ProductAddDTO(
-    title="Headphones",
-    price=999.99,
-    stock_qty=2,
-)
-
-product_data_2 = ProductAddDTO(
-    title="JBL",
-    price=7000,
-    stock_qty=0,
-)
 
 
 class TestCartRepository:
-    async def _create_test_user_and_address(self, user_repository: UserRepository, address_repository: AddressRepository, user_data: UserAddDTO) -> tuple[UserDTO, AddressDTO]:
-        user = await user_repository.create(user_data)
-
-        address_data = AddressAddDTO(
-            user_id=user.id,
-            street="Lenina",
-            city="Ekb",
-            country="Russia",
-        )
-        address = await address_repository.create(address_data)
-
-        return user, address
-
-    async def _create_test_cart(self, cart_repository: CartRepository, user_repository: UserRepository, address_repository: AddressRepository) -> tuple[CartDTO, CartAddDTO]:
-        user, address = await self._create_test_user_and_address(user_repository, address_repository, user_data_1)
-
-        cart_data = CartAddDTO(
-            customer_id=user.id,
-            delivery_address_id=address.id,
-        )
-        cart = await cart_repository.create(cart_data)
-        return cart, cart_data
-
-    async def _create_test_product(self, product_repository: ProductRepository, product_data: ProductAddDTO) -> tuple[ProductDTO, ProductAddDTO]:
-        product = await product_repository.create(product_data)
-        return product, product_data
-
     @pytest.mark.asyncio
-    async def test_create_cart(self, cart_repository: CartRepository, user_repository: UserRepository, address_repository: AddressRepository):
-        cart, cart_data = await self._create_test_cart(cart_repository, user_repository, address_repository)
+    async def test_create_cart(self, cart_repository: CartRepository, cart_data_1: CartAddDTO):
+        cart = await cart_repository.create(cart_data_1)
 
         assert cart.id is not None
-        assert cart.customer_id == cart_data.customer_id
-        assert cart.delivery_address_id == cart_data.delivery_address_id
+        assert cart.customer_id == cart_data_1.customer_id
+        assert cart.delivery_address_id == cart_data_1.delivery_address_id
 
     @pytest.mark.asyncio
-    async def test_update_cart(self, cart_repository: CartRepository, user_repository: UserRepository, address_repository: AddressRepository):
-        cart, _ = await self._create_test_cart(cart_repository, user_repository, address_repository)
-        new_user, new_address = await self._create_test_user_and_address(user_repository, address_repository, user_data_2)
+    async def test_update_cart(self, cart_repository: CartRepository, cart_1: CartDTO, user_2: UserDTO, address_2: UserDTO):
+        new_cart_data = CartUpdateDTO(customer_id=user_2.id, delivery_address_id=address_2.id)
 
-        new_cart_data = CartUpdateDTO(customer_id=new_user.id, delivery_address_id=new_address.id)
+        await cart_repository.update(cart_1, new_cart_data)
 
-        await cart_repository.update(cart, new_cart_data)
-
-        updated_cart = await cart_repository.get_by_id(cart.id)
+        updated_cart = await cart_repository.get_by_id(cart_1.id)
         
-        assert updated_cart.id == cart.id
-        assert updated_cart.customer_id == new_user.id
-        assert updated_cart.delivery_address_id == new_address.id
+        assert updated_cart.id == cart_1.id
+        assert updated_cart.customer_id == user_2.id
+        assert updated_cart.delivery_address_id == address_2.id
 
     @pytest.mark.asyncio
-    async def test_delete_cart(self, cart_repository: CartRepository, user_repository: UserRepository, address_repository: AddressRepository):
-        cart, _ = await self._create_test_cart(cart_repository, user_repository, address_repository)
-        await cart_repository.delete(cart)
+    async def test_delete_cart(self, cart_repository: CartRepository, cart_1: CartDTO):
+        await cart_repository.delete(cart_1)
 
-        found_cart = await cart_repository.get_by_id(cart.id)
+        found_cart = await cart_repository.get_by_id(cart_1.id)
         assert found_cart is None
 
     @pytest.mark.asyncio
-    async def test_add_product(self, cart_repository: CartRepository, user_repository: UserRepository, address_repository: AddressRepository, product_repository: ProductRepository):
-        cart, _ = await self._create_test_cart(cart_repository, user_repository, address_repository)
-        product, _ = await self._create_test_product(product_repository, product_data_1)
+    async def test_add_product(self, cart_repository: CartRepository, cart_1: CartDTO, product_1: ProductDTO):
 
-        cart_item = await cart_repository.add_product(cart, product, qty=2)
+        cart_item = await cart_repository.add_product(cart_1, product_1, qty=2)
 
-        assert cart_item.product_id == product.id
-        assert cart_item.cart_id == cart.id
+        assert cart_item.cart_id == cart_1.id
+        assert cart_item.product_id == product_1.id
         assert cart_item.quantity == 2
 
     @pytest.mark.asyncio
-    async def test_add_same_product_multiple_times(self, cart_repository: CartRepository, user_repository: UserRepository, address_repository: AddressRepository, product_repository: ProductRepository):
-        cart, _ = await self._create_test_cart(cart_repository, user_repository, address_repository)
-        product, _ = await self._create_test_product(product_repository, product_data_1)
-
+    async def test_add_same_product_multiple_times(self, cart_repository: CartRepository, cart_1: CartDTO, product_1: ProductDTO):
         # добавляем продукт первый раз
-        cart_item_1 = await cart_repository.add_product(cart, product, qty=2)
+        cart_item_1 = await cart_repository.add_product(cart_1, product_1, qty=2)
         assert cart_item_1.quantity == 2
 
         # добавляем тот же продукт ещё раз
-        cart_item_2 = await cart_repository.add_product(cart, product, qty=3)
-        assert cart_item_2.product_id == product.id
-        assert cart_item_2.cart_id == cart.id
+        cart_item_2 = await cart_repository.add_product(cart_1, product_1, qty=3)
+        assert cart_item_2.cart_id == cart_1.id
+        assert cart_item_2.product_id == product_1.id
 
         assert cart_item_2.quantity == 5
         assert cart_item_1 is cart_item_2
 
     @pytest.mark.asyncio
-    async def test_get_all_carts(self, cart_repository: CartRepository, user_repository: UserRepository, address_repository: AddressRepository):
-        _, cart_data = await self._create_test_cart(cart_repository, user_repository, address_repository)
-
+    async def test_get_all_carts(self, cart_repository: CartRepository, cart_1: CartDTO, cart_2: CartDTO):
         cart_list = await cart_repository.get_all()
 
         cart_customers = [cart.customer_id for cart in cart_list]
 
-        assert len(cart_list) == 1
-        assert cart_customers[0] == cart_data.customer_id
+        assert len(cart_list) == 2
+        assert cart_customers[0] == cart_1.customer_id
+        assert cart_customers[1] == cart_2.customer_id
