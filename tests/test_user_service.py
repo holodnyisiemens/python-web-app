55e517b898015b94be4914addf8edf9ed98f99a4
holodnyisiemens <idolexey@yandex.ru>
2025-12-09 01:57:29 +0500

    User service tests fixes

--- tests/test_user_service.py
+++ tests/test_user_service.py
@@ -7,15 +7,9 @@ from repositories.user_repository import UserRepository
 from services.user_service import UserService
 
 
-user_data = UserAddDTO(
-    username="example",
-    email="email@example.com"
-)
-
-
 class TestUserService:
     @pytest.mark.asyncio
-    async def test_create_user(self):
+    async def test_create_user(self, user_data_1: UserAddDTO):
         # мокаем репозиторий и объекты в нем
         mock_user_repo = AsyncMock(spec=UserRepository)
         mock_user_repo.session = Mock()
@@ -23,23 +17,23 @@ class TestUserService:
         mock_user_repo.session.rollback = AsyncMock()
 
         # то, что вернёт create()
-        mock_user_repo.create.return_value = {
-            "id": uuid4(),
-            "username": "example",
-            "email": "email@example.com",
-        }
+        mock_user_repo.create.return_value = UserDTO(
+            id=uuid4(),
+            username=user_data_1.username,
+            email=user_data_1.email
+        )
 
         user_service = UserService(user_repo=mock_user_repo)
-        result = await user_service.create(user_data)
+        result = await user_service.create(user_data_1)
 
         # проверка что для асинхронного метода create() был всего 1 await и с указанным объектом
-        mock_user_repo.create.assert_awaited_once_with(user_data)
+        mock_user_repo.create.assert_awaited_once_with(user_data_1)
         mock_user_repo.session.commit.assert_awaited_once()
         assert isinstance(result, UserDTO)
-        assert result.username == "example"
+        assert result.username == user_data_1.username
 
     @pytest.mark.asyncio
-    async def test_delete_user(self):
+    async def test_delete_user(self, user_data_1: UserAddDTO):
         mock_user_repo = AsyncMock(spec=UserRepository)
         mock_user_repo.session = Mock()
         mock_user_repo.session.commit = AsyncMock()
@@ -47,7 +41,12 @@ class TestUserService:
 
         user_id = uuid4()
 
-        fake_user = Mock(id=user_id, username="example", email="email@example.com")
+        fake_user = Mock(
+            id=user_id,
+            username=user_data_1.username, 
+            email=user_data_1.email,
+        )
+
         mock_user_repo.get_by_id.return_value = fake_user
 
         mock_user_repo.delete.return_value = None
