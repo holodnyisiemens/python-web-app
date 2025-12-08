598a7fc34a20cbddcd55c70a675a2d4f5c99a504
holodnyisiemens <idolexey@yandex.ru>
2025-12-08 22:52:33 +0500

    Modify cart_repository

--- src/repositories/cart_repository.py
+++ src/repositories/cart_repository.py
@@ -3,8 +3,9 @@ from uuid import UUID
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
-from models import Cart
+from models import Cart, Product, CartProduct
 from schemas import CartAddDTO, CartUpdateDTO
 
 
@@ -24,28 +25,49 @@ class CartRepository:
         
         return cart
 
-    async def delete(self, cart_id: UUID) -> Optional[Cart]:
-        stmt = select(Cart).where(Cart.id == cart_id)
-        result = await self.session.execute(stmt)
-        cart = result.scalar_one_or_none()
-        if not cart:
-            return None
+    async def delete(self, cart: Cart) -> None:
         await self.session.delete(cart)
-        await self.session.commit()
-        return cart
+        await self.session.flush()
 
-    async def update(self, cart_id: UUID, cart_data: CartUpdateDTO) -> Cart:
-        stmt = select(Cart).where(Cart.id == cart_id)
-        result = await self.session.execute(stmt)
-        cart = result.scalar_one_or_none()
-        if not cart:
-            return None
+    async def update(self, cart: Cart, cart_data: CartUpdateDTO) -> Cart:
         # обновляем только переданные поля
         update_data = cart_data.model_dump(exclude_unset=True)
         
         for field, value in update_data.items():
             setattr(cart, field, value)
 
-        await self.session.commit()
+        await self.session.flush()
         await self.session.refresh(cart)
+
         return cart
+    
+    async def get_cart_with_items(self, cart: Cart) -> Cart:
+        stmt = (
+            select(Cart)
+            .where(Cart.id == cart.id)
+            .options(selectinload(Cart.cart_items))
+        )
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def add_product(self, cart: Cart, product: Product, qty: int = 1) -> CartProduct:
+        # ищем, есть ли уже запись в cart_product
+        cart = await self.get_cart_with_items(cart)
+
+        for item in cart.cart_items:
+            if item.product_id == product.id:
+                item.quantity += qty
+                await self.session.flush()
+                return item
+        
+        new_item = CartProduct(
+            cart_id=cart.id,
+            product_id=product.id,
+            quantity=qty,
+        )
+
+        cart.cart_items.append(new_item)
+        
+        self.session.add(new_item)
+        await self.session.flush()
+        return new_item
