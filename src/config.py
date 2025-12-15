5aa69ded3c5bf0a43bec9e44538aa2d10a37aebd
holodnyisiemens <idolexey@yandex.ru>
2025-12-15 14:08:04 +0500

    Add RabbitMQ configs

--- src/config.py
+++ src/config.py
@@ -9,6 +9,12 @@ class Settings(BaseSettings):
     DB_PASSWORD: SecretStr
     DB_NAME: str
 
+    RABBIT_HOST: str
+    RABBIT_PORT: int
+    RABBIT_USER: str
+    RABBIT_PASSWORD: SecretStr
+    RABBIT_VHOST: str = "/"
+
     @property
     def database_url_asyncpg(self):
         return (
@@ -21,6 +27,13 @@ class Settings(BaseSettings):
         return (
             f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}@"
             f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+    @property
+    def rabbit_url(self):
+        return (
+            f"amqp://{self.RABBIT_USER}:{self.RABBIT_PASSWORD.get_secret_value()}@"
+            f"{self.RABBIT_HOST}:{self.RABBIT_PORT}/{self.RABBIT_VHOST}"
         )
 
     model_config = SettingsConfigDict(
