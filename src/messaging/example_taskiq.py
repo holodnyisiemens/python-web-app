a71ac1fbf43aab07caed13f1fcd0754fdd192839
holodnyisiemens <idolexey@yandex.ru>
2025-12-16 20:34:20 +0500

    Add example_taskiq.py

---
+++ src/messaging/example_taskiq.py
@@ -0,0 +1,28 @@
+from taskiq import TaskiqScheduler
+from taskiq.schedule_sources import LabelScheduleSource
+from taskiq_aio_pika import AioPikaBroker
+
+broker = AioPikaBroker(
+    "amqp://guest:guest@localhost:5672/local",
+    exchange_name="report",  # обменник
+    queue_name="cmd_cart",  # очередь для отправки
+)
+
+scheduler = TaskiqScheduler(
+    broker=broker,
+    sources=[LabelScheduleSource(broker)],
+)
+
+
+@broker.task(
+    schedule=[
+        {
+            "cron": "*/1 * * * *",
+            "args": ["Cron_User"],
+            "schedule_id": "greet_every_mitute",
+        }
+    ]
+)
+async def my_scheduled_task(name: str) -> str:
+    message = f"Scheduled hello to {name} at every minute!"
+    return message
