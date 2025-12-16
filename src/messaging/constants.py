df4f1273ceefd8cee58f0418491d0fb660df20db
holodnyisiemens <idolexey@yandex.ru>
2025-12-16 05:14:21 +0500

    Add event constants

---
+++ src/messaging/constants.py
@@ -0,0 +1,14 @@
+class Events:
+    PRODUCT_CREATED = "product.created"
+    PRODUCT_UPDATED = "product.updated"
+    PRODUCT_DELETED = "product.deleted"
+
+    USER_CREATED = "user.created"
+    USER_UPDATED = "user.updated"
+    USER_DELETED = "user.deleted"
+
+    CART_CREATED = "cart.created"
+    CART_UPDATED = "cart.updated"
+    CART_DELETED = "cart.deleted"
+    CART_ITEM_ADDED = "cart.item_added"
+    CART_ITEM_REMOVED = "cart.item_removed"
